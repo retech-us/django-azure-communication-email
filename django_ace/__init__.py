61623d68cf63b68fbf25a10de93545f64d7efab2
Dmitrii Azarenko <dmitrii@retechlabs.com>
2023-07-18 09:56:14 +0400

    Added CI, extended Python & Django dependencies

--- django_ace/__init__.py
+++ django_ace/__init__.py
@@ -1,4 +1,8 @@
-import importlib_metadata
+try:
+    import importlib_metadata
+except ImportError:
+    # Python >=3.8,<3.10
+    import importlib.metadata as importlib_metadata
 
 from .backend import ACEmailBackend as EmailBackend
 
